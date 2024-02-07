f88625f5fe05c10ec6d33d9b03299b71f00b38b3
Thisara Sandeepa <thisarar46@gmail.com>
2024-02-07 08:56:09 +0530

    Removed the unwanted codelines

--- src/ShoppingGUI.java
+++ src/ShoppingGUI.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ShoppingGUI extends JFrame {
 
